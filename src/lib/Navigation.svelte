6d527a0ff4be72ea0372c05c5a8fb0804ef9bc7d
jd <jd@cic.at>
2024-11-19 13:22:16 +0100

    feat: svelte component testing & testing unchanged maplibre-svelte implementation

---
+++ src/lib/Navigation.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {BadgePlus, Map, SquareChartGantt, UserPen} from "lucide-svelte";
+</script>
+
+<div class="navigation justify-content-center">
+    <div class="row">
+        <div class="col-3">
+            <a href="/">
+                <Map/>
+            </a>
+        </div>
+        <div class="col-3">
+            <a>
+                <SquareChartGantt/>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="/create-event">
+                <BadgePlus/>
+            </a>
+        </div>
+        <div class="col-3">
+            <a href="/my-profile">
+                <UserPen/>
+            </a>
+        </div>
+    </div>
+</div>
