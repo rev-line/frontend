01bdad870388c587180ac751d278544257647104
burnembois <franz15.tb@gmail.com>
2024-12-10 10:39:47 +0100

    feat: Accept AGBs and DSGVO on register
    Display error message on invalid input

--- src/routes/login/newUser/+page.svelte
+++ src/routes/login/newUser/+page.svelte
@@ -5,9 +5,16 @@
     import {userInfoStore, createUserInfo} from "$lib/stores/userInfoStore";
     import {goto} from "$app/navigation";
     import * as Card from "$lib/components/ui/card";
+    import {Label} from "bits-ui";
 
     let email: string = '';
     let password: string = '';
+
+    let accept_agb = false;
+    let accept_dsgvo = false;
+
+    let register_error = false;
+    let error_msg = '';
 
     async function createNewUser(){
         try {
@@ -19,26 +26,60 @@
                 goto('/');
         } catch (error) {
             console.error('Login error:', error);
+            error_msg = '';
+            if (!email.match(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i)) {
+                error_msg += 'Invalid email address. ';
+                email = '';
+            }
+            if (password.length < 8) {
+                error_msg += 'Password must be at least 8 characters long. ';
+                password = '';
+            }
+            register_error = true;
         }
     }
 
 </script>
 
-<div class="flex justify-center items-center h-screen">
-    <Card.Root class="w-1/2 sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4">
-        <Card.Header>
-            <Card.Title>Create new User</Card.Title>
-        </Card.Header>
-        <Card.Content>
-            <form>
-                <Input type="text" bind:value={email} placeholder="Email" />
-                <Input type="password" bind:value={password} placeholder="Password" />
-                <div class="flex justify-around mt-2">
-                    <Button on:click={createNewUser} class={`${buttonVariants({ variant: "outline" })} w-2/5`}>Create User</Button>
-                    <Button href="/" class={`${buttonVariants({ variant: "outline" })} w-2/5`}>Cancle</Button>
-                </div>
-            </form>
-        </Card.Content>
-    </Card.Root>
+<div class="flex justify-center items-center h-screen w-full">
+    <div class="flex flex-col items-center w-full max-w-lg">
+        <Card.Root class="flex-col w-full">
+            <Card.Header>
+                <Card.Title>Create new User</Card.Title>
+            </Card.Header>
+            <Card.Content>
+                <form>
+                    <Input type="text" bind:value={email} placeholder="Email" />
+                    <Input type="password" bind:value={password} placeholder="Password" />
+                    <div class="flex justify-around mt-2 w-full">
+                        {#if accept_agb && accept_dsgvo}
+                            <Button on:click={createNewUser} class={`${buttonVariants({ variant: "outline" })} w-2/5`}>Create User</Button>
+                        {:else}
+                            <Button disabled class={`${buttonVariants({ variant: "outline" })} w-2/5`}>Create User</Button>
+                        {/if}
 
+                        <Button href="/" class={`${buttonVariants({ variant: "outline" })} w-2/5`}>Cancel</Button>
+                    </div>
+                </form>
+                {#if !accept_dsgvo || !accept_agb}
+                    <label for="disabled_submit" class="text-center text-gray-500 text-xs">Please accept our AGBs and DSGVO conditions to continue</label>
+                {/if}
+                <br>
+                <label for="agb">I accept the <a target="_blank" href="https://cdn.programar.io/lAGa5/bODaPOSa61.pdf/raw" class="underline">AGBs</a></label>
+                <input id="agb" type="checkbox" bind:checked={accept_agb}>
+                <br>
+                <label for="dsgvo">I accept the <a target="_blank" href="https://cdn.programar.io/lAGa5/YOkoLinu84.pdf/raw" class="underline">DSGVO conditions</a></label>
+                <input id="dsgvo" type="checkbox" bind:checked={accept_dsgvo}>
+            </Card.Content>
+        </Card.Root>
+
+        {#if register_error}
+            <div class="flex mt-2 justify-center w-full">
+                <div class="flex z-50 gap-8 justify-between items-start py-3 px-4 w-full bg-red-600 border border-b border-gray-200 sm:items-center dark:border-gray-700 lg:py-4 dark:bg-gray-800">
+                    <p>{error_msg}</p>
+                    <button onclick={() => {register_error=false;}}>X</button>
+                </div>
+            </div>
+        {/if}
+    </div>
 </div>
